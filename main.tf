1b140a52bf12ae786b8f5d325234320e34839ab5
mohab58977 <105084710+mohab58977@users.noreply.github.com>
2024-01-03 21:50:56 +0200

    adding terraform code and pipeline

--- main.tf
+++ main.tf
@@ -1,8 +1,34 @@
 provider "azurerm" {
   features {}
+}
 
-  client_id       = "<appId>"
-  client_secret   = "<password>"
-  tenant_id       = "<tenant>"
-  subscription_id = "<Your-Subscription-Id>"
+resource "azurerm_resource_group" "default" {
+  name     = "container-registry-rg"
+  location = "East US 2"
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "kspcontainerregistry"
+  resource_group_name = azurerm_resource_group.default.name
+  location            = "East US 2"
+  sku                 = "Standard"
+  admin_enabled       = true
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_admin_username" {
+  value     = azurerm_container_registry.acr.admin_username
+  sensitive = true
+}
+
+output "acr_admin_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
 }
