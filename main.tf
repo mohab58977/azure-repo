ba8137f90aaaf881acc254e44847bffd249c67d3
mohab58977 <105084710+mohab58977@users.noreply.github.com>
2024-01-03 21:46:39 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+
+  client_id       = "<appId>"
+  client_secret   = "<password>"
+  tenant_id       = "<tenant>"
+  subscription_id = "<Your-Subscription-Id>"
+}
